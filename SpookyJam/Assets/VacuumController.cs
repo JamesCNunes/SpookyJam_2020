5abfaeeeee803ba6b16a5f772cd36ccc24e36944
JamesCNunes <55606935+JamesCNunes@users.noreply.github.com>
2020-10-16 23:47:23 -0400

    Begun Player

---
+++ SpookyJam/Assets/VacuumController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumController : MonoBehaviour
+{
+    public Transform vacuumOrigin;
+    public float suctionStrength = 4f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Vacuumable"  && Input.GetMouseButton(0))
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            Debug.Log("Trig");
+            Vector3 direction = vacuumOrigin.position - other.transform.position  ;
+            rb.useGravity = false; 
+            rb.AddForce(direction.normalized * suctionStrength);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Vacuumable")
+        {
+            other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+        
+    }
+}
