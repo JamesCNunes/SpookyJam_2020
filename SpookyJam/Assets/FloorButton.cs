ab00fb330a273203d60a93be392eb03cf6a6a6fd
JamesCNunes <55606935+JamesCNunes@users.noreply.github.com>
2020-10-17 17:21:55 -0400

    Working on Buttons

---
+++ SpookyJam/Assets/FloorButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FloorButton : MonoBehaviour
+{
+    public UnityEvent WhenDown;
+    public UnityEvent WhenUp;
+
+    void Awake()
+    {
+        if (WhenDown == null)
+            WhenDown = new UnityEvent();
+        if (WhenUp == null)
+            WhenUp = new UnityEvent();
+    }
+}
--- SpookyJam/Assets/VacuumController.cs
+++ SpookyJam/Assets/VacuumController.cs
@@ -5,21 +5,64 @@ using UnityEngine;
 public class VacuumController : MonoBehaviour
 {
     public Transform vacuumOrigin;
+    public Transform holdPoint;
     public float suctionStrength = 4f;
+    public float launchStrength = 10f;
+    public float holdDist = 0.5f;
     public Collider col;
+    GameObject heldObject;
 
-    bool suck = false;
-    bool blow = false;
+    bool suckEnabled = false;
+    bool blowEnabled = false;
+    bool holding = false;
+
+    public float cooldownTime = 1f;
+    float cooldownLaunch;
 
     private void OnTriggerStay(Collider other)
     {
         if(other.tag == "Vacuumable")
         {
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
-            Debug.Log("Trig");
-            Vector3 direction = vacuumOrigin.position - other.transform.position  ;
-            rb.useGravity = false; 
-            rb.AddForce(direction.normalized * suctionStrength);
+            //if sucking
+            if (suckEnabled)
+            {
+                Debug.Log("Sucking");
+                float dist = Vector3.Distance(holdPoint.position, other.transform.position);
+                Debug.Log(dist);
+
+                if (dist <= holdDist && !holding)
+                {
+                    Debug.Log("Now Held");
+                    holding = true;
+                    rb.useGravity = false;
+                    other.transform.position = holdPoint.position;
+                    other.transform.parent = holdPoint;
+                    rb.velocity = Vector3.zero;
+                    rb.angularVelocity = Vector3.zero;
+                    heldObject = other.gameObject;
+                    return;
+                } else if (holding)
+                {
+                    return;
+                }
+
+                Vector3 direction = vacuumOrigin.position - other.transform.position;
+                rb.useGravity = false;
+                rb.AddForce(direction.normalized * suctionStrength);
+            }
+            else if (blowEnabled)
+            {
+                Debug.Log("Blowing");
+                Vector3 direction = other.transform.position - vacuumOrigin.position;
+                rb.useGravity = false;
+                rb.AddForce(direction.normalized * suctionStrength);
+            }
+            else {
+                rb.useGravity = true;
+            }
+            //if blowing
+            
         }
     }
 
@@ -27,19 +70,44 @@ public class VacuumController : MonoBehaviour
     {
         if(other.tag == "Vacuumable")
         {
-            other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            //other.gameObject.GetComponent<Rigidbody>().useGravity = true;
         }
         
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && !blowEnabled)
         {
-            suck = true;
-        } else if (Input.GetMouseButtonUp(0))
+            suckEnabled = true;
+        } else if (Input.GetMouseButtonUp(0) && !blowEnabled)
         {
-            suck = false;
+            suckEnabled = false;
+
+            if (holding)
+            {
+                holding = false;
+                heldObject.transform.parent = null;
+                heldObject.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+
+        if (Input.GetMouseButton(1) && !suckEnabled)
+        {
+            blowEnabled = true;
+        } 
+        else if (Input.GetMouseButton(1) && suckEnabled && holding)
+        {
+            blowEnabled = true;
+            suckEnabled = false;
+            holding = false;
+            heldObject.transform.parent = null;
+            heldObject.GetComponent<Rigidbody>().useGravity = true;
+            heldObject.GetComponent<Rigidbody>().AddForce((holdPoint.position - vacuumOrigin.position) * launchStrength, ForceMode.Impulse);
+        } 
+        else if (Input.GetMouseButtonUp(1) && !suckEnabled)
+        {
+            blowEnabled = false;
         }
     }
 }
